b4347540640f0f925376b1c9665c4c39f8ec9935
joncancode <jc.haines19@gmail.com>
2017-09-13 11:19:34 -0400

    created actions for getting questions

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,55 +1,95 @@
-
-
 export const GET_QUESTION_REQUEST = 'GET_QUESTION_REQUEST';
 export const getQuestionRequest = words => ({
-    type: GET_QUESTION,
-    words
-})
+  type: GET_QUESTION,
+  words
+});
 
 export const GET_QUESTION_SUCCESS = 'GET_QUESTION_SUCCESS';
 export const getQuestionSuccess = words => ({
-    type: GET_QUESTION_SUCCESS,
-    words
-})
+  type: GET_QUESTION_SUCCESS,
+  words
+});
 
 export const GET_QUESTION_ERROR = 'GET_QUESTION_ERROR';
 export const getQuestionError = words => ({
-    type: GET_QUESTION_ERROR,
-    words
-})
+  type: GET_QUESTION_ERROR,
+  words
+});
 
-//GET_SCORE 
+//GET_SCORE
 
 export const PUT_QUESTION_REQUEST = 'PUT_QUESTION_REQUEST';
 export const putQuestionRequest = (words, score) => ({
-    type: PUT_QUESTION_REQUEST,
-    words,
-    score
-})
+  type: PUT_QUESTION_REQUEST,
+  words,
+  score
+});
 
 export const PUT_QUESTION_SUCCESS = 'PUT_QUESTION_SUCCESS';
 export const putQuestionSuccess = (words, score) => ({
-    type: PUT_QUESTION_SUCCESS,
-    words,
-    score
-})
+  type: PUT_QUESTION_SUCCESS,
+  words,
+  score
+});
 
 export const PUT_QUESTION_ERROR = 'PUT_QUESTION_ERROR';
 export const putQuestionError = (words, score) => ({
-    type: PUT_QUESTION_ERROR,
-    words,
-    score
-})
+  type: PUT_QUESTION_ERROR,
+  words,
+  score
+});
+
+// AUTH
+
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const authSuccess = googleId => {
+  type: AUTH_SUCCESS, googleId;
+};
+
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const authError = googleId => {
+  type: AUTH_ERROR, googleId;
+};
+
+export const authenticate = () => dispatch => {
+  const accessToken = Cookies.get('accessToken');
+  if (accessToken) {
+    fetch('/api/me', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          if (res.status !== 401) {
+            // Unauthorized, clear the cookie and go to
+            // the login page
+            Cookies.remove('accessToken');
+            return;
+          }
+          throw new Error(res.statusText);
+        }
+        return res.json();
+        //change under this to redux
+      })
+      .then(currentUser => authSuccess(currentUser));
+  }
+};
 
 export const getQuestion = () => dispatch => {
-    dispatch(getQuestionRequest());
-    fetch('http://localhost:8080/api/questions')
-        .then(res => {
-            if(!res.ok) {
-                console.log('its dead')
-                return Promise.reject(res.statusText)
-            }
-            console.log('hello!')
-            return res.json();
-        })
-}
+  dispatch(getQuestionRequest());
+  fetch('http://localhost:8080/api/questions')
+    .then(res => {
+      if (!res.ok) {
+        console.log('its dead');
+        return Promise.reject(res.statusText);
+      }
+      console.log('hello!');
+      return res.json();
+    })
+    .then(question => {
+      dispatch(getQuestionSuccess(question));
+      console.log(question);
+    })
+    .catch(error => dispatch(getQuestionError(error.message)));
+};
